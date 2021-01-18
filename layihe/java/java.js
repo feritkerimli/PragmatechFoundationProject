b084025d6c3b12b22d89350ef30810698582b221
feritkerimli <feridkerimli1918@gmail.com>
2021-01-18 22:09:37 +0400

    test yoxlama ozek

--- layihe/java/java.js
+++ layihe/java/java.js
@@ -2,11 +2,48 @@ document.getElementById("sign").addEventListener("onclick",onclick);
 document.getElementById("sign").addEventListener("onclick", onclick);
 document.getElementById("giris-fon").addEventListener("onclick",onclick);
 document.getElementById("giris-fon").addEventListener("onclick", onclick);
+document.getElementById("labelduz").addEventListener("onclick", onclick);
+document.getElementById("submit-btn").addEventListener("onclick", onclick);
+document.getElementById("label-c").addEventListener("onclick", onclick);
+document.getElementById("label-a").addEventListener("onclick", onclick);
+
 function tikla(){
   document.getElementById("sign").style.display = "block";
   document.getElementById("giris-fon").style.display = "block";
-}
+};
 function exit(){
     document.getElementById("sign").style.display = "none";
     document.getElementById("giris-fon").style.display = "none";
+};
+
+var duz = false;
+var a= false;
+var b= false;
+var sub_btn=false;
+function yoxlama(){
+  duz = true
+}
+function value_1(){
+  a = true
+}
+function value_2(){
+  c = true
+}
+function testyoxla(){
+  if (duz==true){
+    document.getElementById("labelduz").style.color="green";
+  }
+  else if(a==true){
+    document.getElementById("labelduz").style.color="green";
+    document.getElementById("label-a").style.color="red";
+  }
+  else if(c==true){
+    document.getElementById("labelduz").style.color="green";
+    document.getElementById("label-c").style.color="red";
+  }
+};
+function reset(){
+  duz=false
+  a=false
+  b=false
 }
