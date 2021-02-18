1a7fdc80b2c610164c40c5890cae8d82b7ac96bb
feritkerimli <feridkerimli1918@gmail.com>
2021-02-18 22:39:47 +0400

    Update writefile.py(problem solving)

--- Python_Tasks/File/writefile.py
+++ Python_Tasks/File/writefile.py
@@ -1,9 +1,12 @@
-con=open('Python_Tasks/File/python.txt','a')
+
 print("App`i dayandirmaq ucun : exit yazin")
 while True:
+    con=open('Python_Tasks/File/python.txt','a')
     a= input("Melumat elave edin:")
     if a=="exit":
         break
+        con.close()
     else:
         con.write(a)
         con.write("\n")
+        con.close()
